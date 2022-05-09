aa90d1c14aa1a3d7d4c2275427de50cf012e3cd5
devops <salesfos@outlook.com>
2022-05-09 19:00:49 -0400

    commit on main

--- gke/vars-gcp.tf
+++ gke/vars-gcp.tf
@@ -2,9 +2,9 @@ variable "gcp_project_id" {
   type = string
 }
 
-variable "gcp_credentials" {
-  type = string
-}
+#variable "gcp_credentials" {
+#  type = string
+#}
 
 variable "gcp_region" {
   type = string
