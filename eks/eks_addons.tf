e1c3f6b721aa92f9836209d21c285cdf8d07d8b5
devops <salesfos@outlook.com>
2022-05-11 02:56:29 -0400

    commit on main

--- eks/eks_addons.tf
+++ eks/eks_addons.tf
@@ -1,4 +1,4 @@
 resource "aws_eks_addon" "vpc_cni" {
-  cluster_name = aws_eks_cluster.eks.name
+  cluster_name = aws_eks_cluster.eks[count.index]
   addon_name   = "vpc-cni"
 }
--- eks/output.tf
+++ eks/output.tf
@@ -1,11 +1,11 @@
 output "vpc_id" { value = aws_vpc.eks.id }
 
 output "endpoint" {
-  value = aws_eks_cluster.eks.endpoint
+  value = aws_eks_cluster.eks[*].endpoint
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.eks.certificate_authority[0].data
+  value = aws_eks_cluster.eks[*].certificate_authority[*].data
 
   sensitive = true
 }
