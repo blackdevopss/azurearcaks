3fdb1dfd68b575686cb0d3c7908062aeea674940
devops <salesfos@outlook.com>
2022-05-11 03:14:26 -0400

    commit on main

--- eks/eks_addons.tf
+++ eks/eks_addons.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_addon" "vpc_cni" {
   count        = 1
-  cluster_name = aws_eks_cluster.eks[count.index]
+  cluster_name = element(aws_vpc.eks.*.id, 0)
   addon_name   = "vpc-cni"
 }
