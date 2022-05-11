8ea6282e766be0e5be7d0260b7cde888d17c1f24
devops <salesfos@outlook.com>
2022-05-11 17:08:47 -0400

    commit on main

--- eks/eks_addons.tf
+++ eks/eks_addons.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_addon" "vpc_cni" {
   count        = 1
-  cluster_name = element(aws_subnet.public.*.name, 0)
+  cluster_name = "${local.prefix}-${local.Env}-${local.region_prefix}"
   addon_name   = "vpc-cni"
 
   depends_on = [aws_eks_cluster.eks]
