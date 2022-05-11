dda1786e581da2f3c34c1768b0ea8aa780e0490c
devops <salesfos@outlook.com>
2022-05-11 02:52:17 -0400

    commit on main

--- eks/aws_rtb.tf
+++ eks/aws_rtb.tf
@@ -19,13 +19,13 @@ resource "aws_route_table" "private" {
 // ROUTE TABLE ASSOCIATION
 resource "aws_route_table_association" "public" {
   count          = length(var.eks_private_subnets_cidr)
-  subnet_id      = aws_subnet.public.public_subnet_ids[0]
+  subnet_id      = aws_subnet.public[count.index]
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
   count          = length(var.eks_private_subnets_cidr)
-  subnet_id      = aws_subnet.public.private_subnet_ids[0]
+  subnet_id      = aws_subnet.private[count.index]
   route_table_id = aws_route_table.private.id
 }
 
