3d3203aef0c33d3dd68ddd55ce808c93948c6034
devops <salesfos@outlook.com>
2022-05-11 17:58:16 -0400

    commit on main

--- eks/aws_rtb.tf
+++ eks/aws_rtb.tf
@@ -16,19 +16,6 @@ resource "aws_route_table" "private" {
   }
 }
 
-// ROUTE TABLE ASSOCIATION
-resource "aws_route_table_association" "public" {
-  count          = length(var.eks_private_subnets_cidr)
-  subnet_id      = element(aws_subnet.public[count.index].id)
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "private" {
-  count          = length(data.aws_subnets.private.ids)
-  subnet_id      = data.aws_subnets.private.ids[count.index]
-  route_table_id = aws_route_table.private.id
-}
-
 
 // ROUTES
 resource "aws_route" "internet" {
