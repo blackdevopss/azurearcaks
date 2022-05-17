01980929b762ec13075bf765f05d6bd8ab938721
devops <salesfos@outlook.com>
2022-05-16 23:21:14 -0400

    commit on main

---
+++ clusters/gke/datasource.tf
@@ -0,0 +1,7 @@
+data "google_service_account" "gke" {
+  account_id = "svs-terraform-iac@slsfs-dev.iam.gserviceaccount.com"
+}
+
+output "service_account_id" {
+  value = data.google_service_account.aks.email
+}
---
+++ clusters/gke/eks.tf
@@ -0,0 +1,29 @@
+
+resource "google_container_cluster" "gke" {
+  name     = "gke-poc-clus01"
+  location = "us-central1"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "gke" {
+  name       = "gke-nodepool-poc"
+  location   = "us-central1"
+  cluster    = google_container_cluster.gke.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = data.google_service_account.aks.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
