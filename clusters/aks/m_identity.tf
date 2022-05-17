fc1cacfdd6a3fbe27dcd55503af43cab99e50cf1
devops <salesfos@outlook.com>
2022-05-16 23:33:29 -0400

    commit on main

--- clusters/aks/m_identity.tf
+++ clusters/aks/m_identity.tf
@@ -2,7 +2,7 @@ resource "azurerm_user_assigned_identity" "uai" {
   resource_group_name = azurerm_resource_group.aks.name
   location            = azurerm_resource_group.aks.location
 
-  name = "mi-${azurerm_kubernetes_cluster.aks.name}"
+  name = "mi-aks-poc"
   tags = var.tags
 }
 
