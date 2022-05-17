62437aedd5fac4fd41ea8ba3d8f551f61457cbd9
devops <salesfos@outlook.com>
2022-05-16 23:22:51 -0400

    commit on main

--- clusters/aks/variables.tf
+++ clusters/aks/variables.tf
@@ -45,3 +45,19 @@ variable "azure_region" {
 variable "tags" {
   type = map(string)
 }
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
